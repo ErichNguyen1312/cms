0c1ff2748fb7c3e6a34157c8e08eef76f4c64921
ErichNguyen1312 <vinhnguyenfit1312@gmail.com>
2025-10-12 11:13:55 +0700

    header v1

--- wordpress/wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
+++ wordpress/wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Displays the menus and widgets at the end of the main element.
  * Visually, this output is presented as part of the footer element.
@@ -8,16 +9,16 @@
  * @since Twenty Twenty 1.0
  */
 
-$has_footer_menu = has_nav_menu( 'footer' );
-$has_social_menu = has_nav_menu( 'social' );
+$has_footer_menu = has_nav_menu('footer');
+$has_social_menu = has_nav_menu('social');
 
-$has_sidebar_1 = is_active_sidebar( 'sidebar-1' );
-$has_sidebar_2 = is_active_sidebar( 'sidebar-2' );
+$has_sidebar_1 = is_active_sidebar('sidebar-1');
+$has_sidebar_2 = is_active_sidebar('sidebar-2');
 $has_sidebar_3 = is_active_sidebar('sidebar-3');
 
 // Only output the container if there are elements to display.
-if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) {
-	?>
+if ($has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2) {
+?>
 
 <div class="footer-nav-widgets-wrapper header-footer-group">
 
@@ -25,43 +26,43 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) 
 
     <?php
 
-			$footer_top_classes = '';
+      $footer_top_classes = '';
 
-			$footer_top_classes .= $has_footer_menu ? ' has-footer-menu' : '';
-			$footer_top_classes .= $has_social_menu ? ' has-social-menu' : '';
+      $footer_top_classes .= $has_footer_menu ? ' has-footer-menu' : '';
+      $footer_top_classes .= $has_social_menu ? ' has-social-menu' : '';
 
-			if ( $has_footer_menu || $has_social_menu ) {
-				?>
+      if ($has_footer_menu || $has_social_menu) {
+      ?>
 
 
     <?php } ?>
 
-    <?php if ( $has_sidebar_1 || $has_sidebar_2 ) { ?>
+    <?php if ($has_sidebar_1 || $has_sidebar_2) { ?>
 
     <aside class="footer-widgets-outer-wrapper">
 
       <div class="footer-widgets-wrapper">
 
-        <?php if ( $has_sidebar_1 ) { ?>
+        <?php if ($has_sidebar_1) { ?>
 
         <div class="footer-widgets column-one grid-item">
-          <?php dynamic_sidebar( 'sidebar-1' ); ?>
+          <?php dynamic_sidebar('sidebar-1'); ?>
         </div>
 
         <?php } ?>
 
-        <?php if ( $has_sidebar_2 ) { ?>
+        <?php if ($has_sidebar_2) { ?>
 
         <div class="footer-widgets column-two grid-item">
-          <?php dynamic_sidebar( 'sidebar-2' ); ?>
+          <?php dynamic_sidebar('sidebar-2'); ?>
         </div>
 
         <?php } ?>
 
-        <?php if ( $has_sidebar_3 ) { ?>
+        <?php if ($has_sidebar_3) { ?>
 
         <div class="footer-widgets column-two grid-item">
-          <?php dynamic_sidebar( 'sidebar-3' ); ?>
+          <?php dynamic_sidebar('sidebar-3'); ?>
         </div>
 
         <?php } ?>
